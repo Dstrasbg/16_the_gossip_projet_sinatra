99d9095eb1095959c869218b7fcbc62f18cd976a
dstrasbg <argudo.daniel@gmail.com>
2019-10-21 22:39:28 +0200

    v avec dossier lib

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,9 +1,10 @@
 require 'gossip'
 
 class ApplicationController < Sinatra::Base
-  #prend les 
+  #prend les caracteristiques de Sinatra
+  
   get '/' do
-   erb :index, locals: {gossips: Gossip.all}
+    erb :index, locals: {gossips: Gossip.all}
   end
 
   get '/gossips/new/' do
@@ -12,9 +13,18 @@ class ApplicationController < Sinatra::Base
 
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
-     
-  # ton super code qui enregistre un gossip en fonction de params
-  redirect '/'
-end
+    #enregistre un gossip en fonction de params
+    redirect '/'
+  end
+
+  get '/gossips/:id/' do
+  	erb :show, locals: {gossip: Gossip.find(params['id'])}
+  end
+
+	post '/gossips/:id/edit/' do
+  	Gossip.update(params["modified_author"], params["modified_content"], params['id'])	
+		redirect '/'
+	end
+
 
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,10 +1,11 @@
 class Gossip
   
-  attr_accessor :author, :content #habilité à lire et écrire
+  attr_accessor :author, :content, :id #habilité à lire et écrire
 
   def initialize(author,content)
     @author = author
     @content = content
+    
   end
   
   def save
@@ -14,13 +15,24 @@ class Gossip
   end
 
   def self.all
-    all_gossips = []
-    CSV.read("./db/gossip.csv").each do |csv_line|
-      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    all_gossips = []# va chercher chacune des lignes du csv do
+    CSV.read("./db/gossip.csv").each do |csv_line| # crée un gossip avec les infos de la ligne
+      @all_gossips << Gossip.new(csv_line[0], csv_line[1])# all_gossips << gossip qui vient d'être créé
     end
-    return all_gossips
+    return @all_gossips
+  end
+
+  def self.find(x)
+    return self.all[x.to_i-1]
   end
   
+  def self.update(modified_author, modified_content, id)
+		modified_gossips = Gossip.all
+		CSV.open("./db/gossip.csv", 'w') 
+		modified_gossips[id.to_i-1].author = modified_author
+		modified_gossips[id.to_i-1].content = modified_content
+		modified_gossips.each {|gossip| gossip.save}
+	end
 
 
 end
